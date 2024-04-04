4a2ef8a6da5ae2c23d44a987c3bb91171282dec3
sike25 <97693483+sike25@users.noreply.github.com>
2024-04-04 19:43:08 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -27,8 +27,8 @@ fi
 echo "## 🤔 LAST UPDATED ON: ${currentTimestamp}" >update.md
 
 # Configure local Git user information
-git config --local user.email "73966883+zhafranzainal@users.noreply.github.com"
-git config --local user.name "Zhafran Zainal"
+git config --local user.email "oogieva25@amherst.edu"
+git config --local user.name "sike25"
 
 # Commit changes to the repository with a random message and the current timestamp
 # git commit -am: commit all modified files and provide a commit message in a single command
